0234bd62ba374ed55488f036312dc4d4c8959f61
aaa2002 <alexand2002@gmail.com>
2023-07-15 19:04:27 +0300

    Commit #5 - video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import './Intro.css';
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+        ref={vidRef}
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle clex__center"
+          onClick={handleVideo}
+        >
+          {playVideo ? (
+            <BsPauseFill className="app__video-overlay_btn" color="#fff" fontSize={30} />
+          ) : (
+            <BsFillPlayFill className="app__video-overlay_btn" color="#fff" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
